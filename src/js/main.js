82b4e89e71251a45fa46edbe3095edda970b82b9
steffDev144 <steffdevoff@hotmail.com>
2023-02-24 23:09:30 +0500

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+$('.main-slider__block').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/header-component/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/header-component/arrow-right.png" alt="next"></button>',
+});
+
 $('.popular__wrapper').slick({
     dots: false,
     prevArrow: '<button type="button" class="slick-prev"><img src="../img/popular-products/prevArrow.png"></button>',
@@ -32,13 +41,40 @@ $('.popular__wrapper').slick({
         }
     ]
 });
-$('.main-slider__block').slick({
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    slidesToShow: 1,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/header-component/arrow-left.png" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/header-component/arrow-right.png" alt="next"></button>',
+
+$('.inner').slick({
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/popular-products/prevArrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/popular-products/nextArrow.png"></button>',
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
 
 function mainCatalogOverlay(catalog, btn, close, active) {
